76b2b30998a54beb48b99832313230f5b3584d9d
cheetamaru <vitalvat@gmail.com>
2023-06-13 21:40:12 +0300

    feat: make a temporal array state of bingo items

--- src/components/BingoField.tsx
+++ src/components/BingoField.tsx
@@ -1,28 +1,55 @@
 import BingoBlock from '@/components/BingoBlock'
+import { useState } from 'react';
+
+type BingoItem = {
+    key: string;
+    content: string;
+}
 
 const rows = 5;
 const cols = 5;
 
-const items = Array(rows).fill(null).map(() => [...Array(cols).fill(null)])
-
-const getCols = (row: null[], rowIndex: number) => {
-    return row.map((col, colIndex) => {
-        return <BingoBlock onClick={() => console.log('clicked', `${rowIndex}/${colIndex}`)} key={`${rowIndex}${colIndex}`}>
-            {rowIndex}{colIndex}
-        </ BingoBlock>
-    })
+const getInitialItems = (): BingoItem[][] => {
+    return Array(rows)
+        .fill(null)
+        .map((_, rowIndex) => [
+            ...Array(cols).fill(null).map((_, colIndex) => ({key: `${rowIndex}${colIndex}`, content: `${rowIndex}/${colIndex}`}))
+        ])
 }
 
-const getEmptyBlocks = () => {
-    return items.map((row, rowIndex) => {
-        return <div key={rowIndex}>
-            {getCols(row, rowIndex)}
-        </div>
-    })
-}
 
 export default function BingoField() {
+    const [items, setItems] =  useState(getInitialItems())
+
+    const getCols = (row: BingoItem[]) => {
+        return row.map(({key, content}) => {
+            return <BingoBlock onClick={() => console.log('clicked', `${content}`)} key={key}>
+                {content}
+            </ BingoBlock>
+        })
+    }
+
+    const getEmptyBlocks = () => {
+        return items.map((row, rowIndex) => {
+            return <div key={rowIndex}>
+                {getCols(row)}
+            </div>
+        })
+    }
+
+    const onClick = () => {
+        const newItems = [...items]
+
+        newItems[0][0] = {
+            key: newItems[0][0].key,
+            content: "new"
+        }
+
+        setItems(newItems)
+    }
+
     return <>
+        <button onClick={onClick}>Change</button>
         {getEmptyBlocks()}
     </>
 }
