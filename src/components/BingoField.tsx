97c3c439af6c78f830ac4094cf6ad19dd2e0fa6b
cheetamaru <vitalvat@gmail.com>
2023-06-16 21:03:38 +0300

    feat: add crude edit functionality

--- src/components/BingoField.tsx
+++ src/components/BingoField.tsx
@@ -4,37 +4,75 @@ import { useState } from 'react';
 type BingoItem = {
     key: string;
     content: string;
+    xCoord?: number;
+    yCoord?: number;
+    index?: number;
 }
 
 const rows = 5;
 const cols = 5;
 
-const getInitialItems = (): BingoItem[][] => {
-    return Array(rows)
-        .fill(null)
-        .map((_, rowIndex) => [
-            ...Array(cols).fill(null).map((_, colIndex) => ({key: `${rowIndex}${colIndex}`, content: `${rowIndex}/${colIndex}`}))
-        ])
+const total = rows * cols;
+
+const getInitialArray = (): BingoItem[] => {
+    return Array(total).fill(null).map((el, index) => {
+        return {
+            key: String(index),
+            content: `${index}`,
+            index,
+        }
+    })
+}
+
+const arrayToMatrix = (arr: BingoItem[]): BingoItem[][] => {
+    const matrix: BingoItem[][] = []
+
+    let k = -1
+
+    arr.forEach((el, index) => {
+        if (index % cols === 0) {
+            k++
+            matrix[k] = []
+        }
+
+        matrix[k].push(el);
+    })
+
+    return matrix
 }
 
 
 export default function BingoField() {
-    const [items, setItems] =  useState(getInitialItems())
+    const [items, setItems] =  useState(getInitialArray())
 
-    const handleClick = (content: BingoItem['content']) => {
-        console.log(content)
+    const handleClick = (content: BingoItem) => {
+        const newItems = items.map((item) => {
+            if (content.index === item.index) {
+                return {
+                    ...item,
+                    content: "changed!"
+                }
+            }
+
+            return item
+        })
+
+        setItems(newItems)
     }
 
     const getCols = (row: BingoItem[]) => {
-        return row.map(({key, content}) => {
-            return <BingoBlock onClick={() => handleClick(content)} key={key}>
+        return row.map((block) => {
+            const {key, content} = block
+            return <BingoBlock onClick={() => handleClick(block)} key={key}>
                 {content}
             </ BingoBlock>
         })
     }
 
     const getEmptyBlocks = () => {
-        return items.map((row, rowIndex) => {
+        const matrix = arrayToMatrix(items)
+
+        return matrix.map((row, rowIndex) => {
             return <div key={rowIndex}>
                 {getCols(row)}
             </div>
